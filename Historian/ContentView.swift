5b283be3726174718147957decbf69d9a57c48cf
Sven A. Schmidt <sas@finestructure.co>
2020-03-15 13:08:18 +0100

    Initial Commit

---
+++ Historian/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Historian
+//
+//  Created by Sven A. Schmidt on 15/03/2020.
+//  Copyright © 2020 finestructure. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
