52eb4c1c8a0a0d63ff2c93b2ddd45a343a67eb3b
Sven A. Schmidt <sas@finestructure.co>
2020-03-16 11:54:07 +0100

    Add macos app stub

---
+++ Historian-macos/Historian/Historian/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Historian
+//
+//  Created by Sven A. Schmidt on 16/03/2020.
+//  Copyright © 2020 finestructure. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
